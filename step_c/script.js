3f3cf475b61051495c93f43b84000a1e48e49a1e
Taher Ahmed Muhammad Taher Ahmed <taher139010@stemegypt.edu.eg>
2018-11-15 20:47:56 -0600

    added search

--- step_c/script.js
+++ step_c/script.js
@@ -4,40 +4,43 @@ function idIndex(a,id) {
     return null;
 }
 
-var xmlhttp = new XMLHttpRequest();
-var url = "http://localhost:11002/db/data/transaction/commit";
-var query_com = {
-    "statements": [
-        {
-            "statement": 'MATCH path = (nebraska{state: "Nebraska"})-[r]->(friend) RETURN path',
-            "resultDataContents": ["graph"]
-        }
-    ]
-};
-var query_str = JSON.stringify(query_com);
-xmlhttp.open("post", url, true);
-xmlhttp.setRequestHeader("Accept", "application/json;charset=UTF-8");
-xmlhttp.setRequestHeader("Content-type", "application/json");
-xmlhttp.send(query_str);
+$('#search').on('click', function () {
+    var query = $('#query').val();
+    var xmlhttp = new XMLHttpRequest();
+    var url = "http://localhost:11002/db/data/transaction/commit";
+    var query_com = {
+        "statements": [
+            {
+                "statement": query,
+                "resultDataContents": ["graph"]
+            }
+        ]
+    };
+    var query_str = JSON.stringify(query_com);
+    xmlhttp.open("post", url, true);
+    xmlhttp.setRequestHeader("Accept", "application/json;charset=UTF-8");
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(query_str);
 
-xmlhttp.onreadystatechange = function () {
-    if(xmlhttp.responseText !== ""){
-        var res = eval("("+xmlhttp.responseText+")");
-        var nodes=[], links=[];
-        res.results[0].data.forEach(function (row) {
-            row.graph.nodes.forEach(function (n) {
-                if (idIndex(nodes,n.id) == null)
-                    nodes.push({id:n.id,label:n.labels[0],title:n.properties.name});
+    xmlhttp.onreadystatechange = function () {
+        if(xmlhttp.responseText !== ""){
+            var res = eval("("+xmlhttp.responseText+")");
+            var nodes=[], links=[];
+            res.results[0].data.forEach(function (row) {
+                row.graph.nodes.forEach(function (n) {
+                    if (idIndex(nodes,n.id) == null)
+                        nodes.push({id:n.id,label:n.labels[0],title:n.properties.name});
+                });
+                links = links.concat( row.graph.relationships.map(function(r) {
+                    return {source:idIndex(nodes,r.startNode),target:idIndex(nodes,r.endNode),type:r.type};
+                }));
             });
-            links = links.concat( row.graph.relationships.map(function(r) {
-                return {source:idIndex(nodes,r.startNode),target:idIndex(nodes,r.endNode),type:r.type};
-            }));
-        });
-        console.log(nodes, links)
-        renderGraph(nodes, links);
-    }
-};
+            console.log(nodes, links)
+            renderGraph(nodes, links);
+        }
+    };
 
+});
 function renderGraph(nodes, links) {
     var width = 800, height = 800;
     var force = d3.layout.force()
