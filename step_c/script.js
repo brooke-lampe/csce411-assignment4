ed78ccdb0d650b03d1ffdcdca17c56562bb9bee8
Taher Ahmed Muhammad Taher Ahmed <taher139010@stemegypt.edu.eg>
2018-11-15 21:44:28 -0600

    first query works

--- step_c/script.js
+++ step_c/script.js
@@ -3,11 +3,14 @@ function idIndex(a,id) {
         if (a[i].id == id) return i;}
     return null;
 }
-
-$('#search').on('click', function () {
-    var query = $('#query').val();
-    var xmlhttp = new XMLHttpRequest();
+var xmlhttp = new XMLHttpRequest();
+$('#search').off('click').on('click', function (e) {
     var url = "http://localhost:11002/db/data/transaction/commit";
+    xmlhttp.open("post", url, true);
+    xmlhttp.setRequestHeader("Accept", "application/json;charset=UTF-8");
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+
+    var query = $('#query').val();
     var query_com = {
         "statements": [
             {
@@ -17,30 +20,28 @@ $('#search').on('click', function () {
         ]
     };
     var query_str = JSON.stringify(query_com);
-    xmlhttp.open("post", url, true);
-    xmlhttp.setRequestHeader("Accept", "application/json;charset=UTF-8");
-    xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(query_str);
-
-    xmlhttp.onreadystatechange = function () {
-        if(xmlhttp.responseText !== ""){
-            var res = eval("("+xmlhttp.responseText+")");
-            var nodes=[], links=[];
-            res.results[0].data.forEach(function (row) {
-                row.graph.nodes.forEach(function (n) {
-                    if (idIndex(nodes,n.id) == null)
-                        nodes.push({id:n.id,label:n.labels[0],title:n.properties.name});
-                });
-                links = links.concat( row.graph.relationships.map(function(r) {
-                    return {source:idIndex(nodes,r.startNode),target:idIndex(nodes,r.endNode),type:r.type};
-                }));
-            });
-            console.log(nodes, links)
-            makeGraph(nodes, links);
-        }
-    };
-
+    return false;
 });
+xmlhttp.onreadystatechange = function () {
+    console.log(xmlhttp.responseText);
+    if(xmlhttp.responseText !== ""){
+        var res = eval("("+xmlhttp.responseText+")");
+        var nodes=[], links=[];
+        res.results[0].data.forEach(function (row) {
+            row.graph.nodes.forEach(function (n) {
+                if (idIndex(nodes,n.id) == null)
+                    nodes.push({id:n.id,label:n.labels[0],title:n.properties.name});
+            });
+            links = links.concat( row.graph.relationships.map(function(r) {
+                return {source:idIndex(nodes,r.startNode),target:idIndex(nodes,r.endNode),type:r.type};
+            }));
+        });
+        console.log(nodes.length, links.length);
+        makeGraph(nodes, links);
+        xmlhttp.abort();
+    }
+};
 
 
 function makeGraph(nodes, links) {
